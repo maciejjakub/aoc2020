9c71cd31248e86df38ab667d3cc1254fffef9064
Maciej Miliszewski <maciejmiliszewski@gmail.com>
2020-12-01 18:33:03 +0100

    Fix indentation

--- day01/a.py
+++ day01/a.py
@@ -4,18 +4,18 @@ from itertools import combinations
 import numpy as np
 
 def load_input(path):
-	with open(path, "r") as file:
-		data = list(map(int, file.read().split()))
-	return data
+    with open(path, "r") as file:
+        data = list(map(int, file.read().split()))
+    return data
 
 def puzzle(count):
-	for i in combinations(data, count):
-		if sum(i) == 2020:
-			print(f"The numbers are {i} and the answer is {np.prod(i)}")
+    for i in combinations(data, count):
+        if sum(i) == 2020:
+            print(f"The numbers are {i} and the answer is {np.prod(i)}")
 
 if __name__ == '__main__':
-	data = load_input("./input")
-	print("Puzzle A:")
-	puzzle(2)
-	print("Puzzle B:")
-	puzzle(3)
+    data = load_input("./input")
+    print("Puzzle A:")
+    puzzle(2)
+    print("Puzzle B:")
+    puzzle(3)
